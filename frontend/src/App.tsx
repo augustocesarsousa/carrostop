00df40bf1afd5ad2405cb0b01ff862fb6112f05a
augusto cesar <acs-03@hotmail.com>
2021-11-20 17:34:16 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar />
       <h1>Cars Top</h1>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-secondary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#carrostop-navbar"
+          aria-controls="carrostop-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse"  id="carrostop-navbar">
+          <ul className="navbar-nav offset-md-4 main-menu">
+            <li>
+              <a href="link" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="link">Catálogo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
